7ae1d6ab7117536acd1befeed4882faa51b11ecf
Rolf Madsen <rolf_madsen@keysight.com>
2024-03-12 17:06:04 +0100

    Incremental work.

---
+++ LabViewMemberData.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+namespace OpenTap.Plugins.LabView
+{
+    public class LabViewMemberData : IMemberData
+    {
+        public LabViewMemberData(LabViewTypeData labViewTypeData, ParameterInfo parameterInfo)
+        {
+            
+            Attributes = Array.Empty<object>();
+            if (parameterInfo.IsOut)
+            {
+                Attributes = new[]
+                {
+                    new OutputAttribute()
+                };
+            }
+            Name = parameterInfo.Name;
+            DeclaringType = labViewTypeData;
+            TypeDescriptor = TypeData.FromType(parameterInfo.ParameterType);
+            Writable = true;
+            Readable = true;
+        }
+
+        public IEnumerable<object> Attributes
+        {
+            get;
+        }
+        public string Name
+        {
+            get;
+        }
+        public void SetValue(object owner, object value)
+        {
+            ((LabViewTestStep)owner).Values[this.Name] = value;
+        }
+        public object GetValue(object owner)
+        {
+            if (((LabViewTestStep)owner).Values.TryGetValue(this.Name, out var current))
+            {
+                return current;
+            }
+            return null;
+        }
+        public ITypeData DeclaringType
+        {
+            get;
+        }
+        public ITypeData TypeDescriptor
+        {
+            get;
+        }
+        public bool Writable
+        {
+            get;
+        }
+        public bool Readable
+        {
+            get;
+        }
+    }
+}
---
+++ LabViewTestStep.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+namespace OpenTap.Plugins.LabView
+{
+    public class LabViewTestStep : TestStep
+    {
+        public ITypeData PluginType;
+        public LabViewTestStep(ITypeData type)
+        {
+            this.PluginType = type;
+            Values = new Dictionary<string, object>();
+        }
+        [Browsable(false)]
+        public Dictionary<string,object> Values
+        {
+            get;
+            set;
+        }
+        public override void Run()
+        {
+            
+        }
+    }
+}
